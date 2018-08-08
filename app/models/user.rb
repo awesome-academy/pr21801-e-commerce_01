7dd1009dfa27ea504012939235b8f238af9f07b9
shjn0 <nguyentung.pdu@gmail.com>
2018-08-08 20:26:45 +0700

    ref Task#8 signup - login - logout - update (user)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
   has_many :likes
   has_many :ratings
   has_many :comments
   has_many :orders
+
+  validates_presence_of :name
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180806140414) do
+ActiveRecord::Schema.define(version: 20180808125705) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -117,6 +117,16 @@ ActiveRecord::Schema.define(version: 20180806140414) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
