d1a68c1b2d70ddf0701318b51183a6ec720f4da5
tdtrung5893 <tranducha23081967@gmail.com>
2018-09-10 21:05:19 +0700

    ref Task#3 show order

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,18 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  host = "localhost:3000"
+  config.action_mailer.default_url_options = { host: host, protocol: "http" }
+  config.action_mailer.smtp_settings = {
+    :address        => "smtp.gmail.com",
+    :port           => "587",
+    :authentication => :plain,
+    :user_name      => ENV["GMAIL_ACCOUNT"],
+    :password       => ENV["GMAIL_PASSWORD"],
+    :domain         => "gmail.com",
+    :enable_starttls_auto => true
+  }
 
   config.action_mailer.perform_caching = false
 
