c56eb1ea7a607541275a2fe9f9f5d2b7ea4a5c78
tdtrung5893 <tranducha23081967@gmail.com>
2018-09-17 10:49:15 +0700

    update I18n

--- config/routes.rb
+++ config/routes.rb
@@ -5,24 +5,26 @@ Rails.application.routes.draw do
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"},
     controllers: {omniauth_callbacks: "users/omniauth_callbacks",
       registrations: "registrations"}
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
 
-  concern :paginatable do
-    get "(page/:page)", action: :index, on: :collection, as: ""
-  end
+    concern :paginatable do
+      get "(page/:page)", action: :index, on: :collection, as: ""
+    end
 
-  resources :carts
-  resources :orders
-  resources :categories, only: :show
-  resources :ratings
-  resources :products, only: :show do
-    resources :comments, only: [:create, :destroy]
-    resource :like, module: :products
-  end
+    resources :carts
+    resources :orders
+    resources :categories, only: :show
+    resources :ratings
+    resources :products, only: :show do
+      resources :comments, only: [:create, :destroy]
+      resource :like, module: :products
+    end
 
-  namespace :admin do
-    resources :categories, concerns: :paginatable
-    resources :images
-    resources :products, concerns: :paginatable
-    resources :users, concerns: :paginatable
+    namespace :admin do
+      resources :categories, concerns: :paginatable
+      resources :images
+      resources :products, concerns: :paginatable
+      resources :users, concerns: :paginatable
+    end
   end
 end
